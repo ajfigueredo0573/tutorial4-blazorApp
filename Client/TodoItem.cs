eeaf3cfef5f39c90ae3c6d99331aec1d13b6349d
Alejandro Figueredo <ajfigueredo0573@eagle.fgcu.edu>
2021-10-04 19:11:29 -0400

    Comment all line of code for todo.razor

---
+++ Client/TodoItem.cs
@@ -0,0 +1,5 @@
+﻿public class TodoItem       //creates a new class
+{
+    public string Title { get; set; }           // Property of the class
+    public bool IsDone { get; set; }            // Property of the class 
+}
